459c2ea840dcce685b0354c8df4e341bb1ef3028
jangjunha <hi@jangjunha.me>
2023-09-07 08:07:11 +0900

    content: 이 지금

--- packages/web/songs/index.ts
+++ packages/web/songs/index.ts
@@ -1,5 +1,6 @@
 import bbibbi from "./bbibbi";
 import blueming from "./blueming";
+import dlwlrma from "./dlwlrma";
 import you_and_i from "./you-and-i";
 
-export default [blueming, bbibbi, you_and_i];
+export default [blueming, bbibbi, dlwlrma, you_and_i];
