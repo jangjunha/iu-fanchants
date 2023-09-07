6e3b19fa308568234d1d5425e7b45ae1fc5bd248
jangjunha <hi@jangjunha.me>
2023-09-07 19:50:02 +0900

    docs: add licenses

---
+++ README.md
@@ -0,0 +1,21 @@
+# 아이유 응원법 초성 리듬게임
+
+리듬게임으로 즐기는 아이유 응원법
+
+<https://iu-fanchants.web.app/>
+
+### Prerequisites
+
+- [Yarn Berry](https://github.com/yarnpkg/berry)
+
+### 실행
+
+```console
+$ yarn workspace iu-fanchants-web dev
+```
+
+### Export licenses
+
+```console
+$ yarn licenses generate-disclaimer -R > packages/web/public/licenses.txt
+```
--- packages/web/app/about/page.tsx
+++ packages/web/app/about/page.tsx
@@ -11,7 +11,7 @@ const AboutPage = (): React.ReactElement => (
         아이유 응원법
       </Link>
     </header>
-    <section className="flex flex-col gap-4 my-4">
+    <section className="flex flex-col gap-8 mt-8 my-4">
       <p>이 사이트는 팬사이트입니다. 아이유나 소속사와 관련이 없습니다.</p>
       <p>
         이 사이트는{" "}
@@ -24,6 +24,32 @@ const AboutPage = (): React.ReactElement => (
         </a>
         를 사용하여 이용 데이터를 수집합니다. 수집한 데이터는 사이트 이용 분석
         목적으로만 사용하며 광고 등의 목적으로 사용하지 않습니다.
+      </p>
+      <p>
+        <Link href="/licenses.txt">오픈 소스 라이센스 정보 (링크)</Link>
+      </p>
+      <p>
+        폰트
+        <ul className="ml-4">
+          <li>
+            <Link
+              href="https://rsms.me/inter/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Inter (Rasmus Andersson)
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://noonnu.cc/font_page/1136"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              오뮤 다예쁨체 (오뮤다이어리, (주)보이저엑스)
+            </Link>
+          </li>
+        </ul>
       </p>
       <p>
         GitHub:{" "}
