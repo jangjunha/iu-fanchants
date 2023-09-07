97e7e6cd052fa03a160f57dd9eacdcb3ea948f10
jangjunha <hi@jangjunha.me>
2023-09-07 09:12:20 +0900

    content: 있잖아

--- packages/web/songs/index.ts
+++ packages/web/songs/index.ts
@@ -3,5 +3,6 @@ import blueming from "./blueming";
 import dlwlrma from "./dlwlrma";
 import everyEndOfTheDay from "./every-end-of-the-day";
 import youAndI from "./you-and-i";
+import youKnow from "./you-know";
 
-export default [blueming, bbibbi, dlwlrma, everyEndOfTheDay, youAndI];
+export default [blueming, bbibbi, dlwlrma, everyEndOfTheDay, youAndI, youKnow];
