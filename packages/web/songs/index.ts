2309fd4b4226d26b826803fd1aa49c8ebe90dc07
jangjunha <hi@jangjunha.me>
2023-09-21 12:03:28 +0900

    content: 스물셋

--- packages/web/songs/index.ts
+++ packages/web/songs/index.ts
@@ -5,12 +5,14 @@ import everyEndOfTheDay from "./every-end-of-the-day";
 import youAndI from "./you-and-i";
 import youKnow from "./you-know";
 import lilac from "./lilac";
+import twentyThree from "./twenty-three";
 
 export default [
   lilac,
   blueming,
   bbibbi,
   dlwlrma,
+  twentyThree,
   everyEndOfTheDay,
   youAndI,
   youKnow,
